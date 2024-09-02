8db98e3ff27f5d701319fb0d14ccfc5d11477432
Jo Eun Hyeong <whdmsgud8@naver.com>
2024-09-02 11:58:33 +0900

    first commit

---
+++ src/main/java/com/team24/newsfeed/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.team24.newsfeed.security;
+
+
+import com.team24.newsfeed.domain.User;
+import com.team24.newsfeed.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
